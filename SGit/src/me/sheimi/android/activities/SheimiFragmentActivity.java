ced1f62a94a2c71c85292aac789d8f647773b55b
Benjamin Tan <demoneaux@gmail.com>
2014-12-10 21:08:58 +0800

    Update Universal Image Loader to v1.9.3

--- SGit/src/me/sheimi/android/activities/SheimiFragmentActivity.java
+++ SGit/src/me/sheimi/android/activities/SheimiFragmentActivity.java
@@ -17,7 +17,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.nostra13.universalimageloader.cache.disc.impl.TotalSizeLimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedLimitedDiscCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -230,7 +230,8 @@ public class SheimiFragmentActivity extends Activity {
         File cacheDir = StorageUtils.getCacheDirectory(this);
         ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(
                 this).defaultDisplayImageOptions(mDiskCacheOption)
-                .discCache(new TotalSizeLimitedDiscCache(cacheDir, SIZE))
+                .discCache(new UnlimitedDiscCache(cacheDir))
+                .diskCacheSize(SIZE)
                 .build();
         mImageLoader = ImageLoader.getInstance();
         mImageLoader.init(configuration);
