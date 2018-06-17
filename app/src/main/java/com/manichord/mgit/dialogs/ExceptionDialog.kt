9f5625e20f4e5ee95679ba0b297c37ec272814a3
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-06-17 13:39:48 +0200

    Revert "Optionally show exception stacktrace"
    This reverts commit e5efe84db9fbe41cbf4bc19e338745ce548c46c1.

--- app/src/main/java/com/manichord/mgit/dialogs/ExceptionDialog.kt
+++ app/src/main/java/com/manichord/mgit/dialogs/ExceptionDialog.kt
@@ -2,11 +2,8 @@ package com.manichord.mgit.dialogs
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.graphics.Typeface
 import android.os.Bundle
 import android.support.annotation.StringRes
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.dialog_exception.view.*
 import me.sheimi.android.views.SheimiDialogFragment
@@ -28,12 +25,6 @@ class ExceptionDialog : SheimiDialogFragment() {
         val inflater = rawActivity.layoutInflater
         val layout = inflater.inflate(R.layout.dialog_exception, null)
         layout.error_message.setText(mErrorRes)
-        layout.show_stacktrace.setOnClickListener {
-            layout.show_stacktrace.visibility = View.GONE
-            layout.error_message.text = throwableString
-            layout.error_message.typeface = Typeface.MONOSPACE
-            layout.error_message.setHorizontallyScrolling(true)
-        }
 
         builder.setView(layout)
 
@@ -68,6 +59,4 @@ class ExceptionDialog : SheimiDialogFragment() {
     fun setErrorRes(@StringRes errorRes: Int) {
         mErrorRes = errorRes
     }
-
-    private val throwableString get() = Log.getStackTraceString(mThrowable)
 }
