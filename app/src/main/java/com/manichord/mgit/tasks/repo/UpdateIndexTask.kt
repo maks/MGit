e2607f6fd719fe3b5a3f726b59c0a41234bc9fb6
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-07-12 06:33:59 +0200

    Fix DirCache unlocking try..finally range to unlock it if an exception occurs

--- app/src/main/java/com/manichord/mgit/tasks/repo/UpdateIndexTask.kt
+++ app/src/main/java/com/manichord/mgit/tasks/repo/UpdateIndexTask.kt
@@ -13,7 +13,7 @@ import org.eclipse.jgit.lib.FileMode
 class UpdateIndexTask(repo: Repo, private val path: String, private val newMode: Int) : RepoOpTask(repo) {
     companion object {
         fun calculateNewMode(executable: Boolean): Int =
-                if(executable) 0b111101101 else 0b110100100 // no octal literals in Kotlin, 0o755 and 0o644
+                if (executable) 0b111101101 else 0b110100100 // no octal literals in Kotlin, 0o755 and 0o644
     }
 
 
@@ -31,15 +31,15 @@ class UpdateIndexTask(repo: Repo, private val path: String, private val newMode:
             return false
         }
 
-        val dirCacheEntry = dircache[path]
-        val entry = if (dirCacheEntry != null) dirCacheEntry else {
-            setException(NoSuchIndexPathException(path), R.string.error_file_not_found)
-            return false
-        }
-        val oldMode = entry.fileMode
-        entry.fileMode = FileMode.fromBits(newMode or (oldMode.bits or 0b111111111 xor 0b111111111))
-
         try {
+            val dirCacheEntry = dircache[path]
+            val entry = if (dirCacheEntry != null) dirCacheEntry else {
+                setException(NoSuchIndexPathException(path), R.string.error_file_not_found)
+                return false
+            }
+            val oldMode = entry.fileMode
+            entry.fileMode = FileMode.fromBits(newMode or (oldMode.bits or 0b111111111 xor 0b111111111))
+
         } finally {
             dircache.unlock()
         }
