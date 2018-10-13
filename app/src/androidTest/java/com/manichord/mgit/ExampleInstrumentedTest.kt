728568d0fb5ccc647363a52605c4dfad283ec564
Maksim Lin <maks@manichord.com>
2018-10-13 17:13:14 +1100

    update Kotlin version, add lint, cleanup code

--- app/src/androidTest/java/com/manichord/mgit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/manichord/mgit/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.manichord.mgit
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/main/java/com/manichord/mgit/MainActivity.kt
+++ app/src/main/java/com/manichord/mgit/MainActivity.kt
@@ -2,7 +2,7 @@ package com.manichord.mgit
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
