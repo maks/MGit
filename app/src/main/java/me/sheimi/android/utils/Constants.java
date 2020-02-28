8912aada5031ad42c9db8029515433f7f44bc3c0
Maksim Lin <maks@manichord.com>
2020-02-28 11:19:34 +1100

    update Gravatar url to HTTPS, rm dead code

--- app/src/main/java/me/sheimi/android/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package me.sheimi.android.utils;
-
-/**
- * Created by sheimi on 8/23/13.
- */
-public class Constants {
-    public static final String DONATE_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=KWFGX7RNJ6LM8&lc=US&item_name=Donate%20SGit&item_number=sgit&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted";
-}
