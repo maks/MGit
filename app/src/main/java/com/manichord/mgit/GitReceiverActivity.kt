5289e8c1b7edfd0a438c70d23704f06e888bdfde
Sandorex <rzhw3h@gmail.com>
2022-04-25 23:22:24 +0200

    Scrapped the broadcast receiver, now using an activity The broadcast receiver had too many problems, now im catching uri with mget:// schema and it works quite nicely

--- app/src/main/java/com/manichord/mgit/GitReceiverActivity.kt
+++ app/src/main/java/com/manichord/mgit/GitReceiverActivity.kt
@@ -1,24 +1,55 @@
 package com.manichord.mgit
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.util.Log
-import android.widget.Toast
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import me.sheimi.android.activities.SheimiFragmentActivity
 import me.sheimi.sgit.R
 import me.sheimi.sgit.database.RepoDbManager
 import me.sheimi.sgit.database.models.Repo
 import me.sheimi.sgit.repo.tasks.SheimiAsyncTask
 import me.sheimi.sgit.repo.tasks.repo.*
 
-class Receiver : BroadcastReceiver() {
-    private val notificationChannel = "com.manichord.mgit.Receiver"
+class GitReceiverActivity : SheimiFragmentActivity() {
+    companion object {
+        // local path of the repository starting from the root directory set in MGit (string)
+        const val EXTRA_REPO_LOCAL_PATH = "local_path"
+
+        // id of the repository, starting from 1 (long)
+        const val EXTRA_REPO_ID = "id"
+
+        // remote to push to, defaults to origin (string)
+        const val EXTRA_REMOTE = "remote"
+
+        // used for both push and pull (string)
+        const val EXTRA_FORCE = "force"
+
+        const val EXTRA_PUSH_ALL = "push_all"
+        const val EXTRA_STAGE_ALL = "stage_all"
+        const val EXTRA_AMEND = "amend"
+        const val EXTRA_COMMIT_MSG = "commit_msg"
+        const val EXTRA_AUTHOR_NAME = "author_name"
+        const val EXTRA_AUTHOR_EMAIL = "author_email"
+        const val EXTRA_FILE_PATTERN = "file_pattern"
+        const val EXTRA_BRANCH = "branch"
+        const val EXTRA_COMMIT = "commit"
+
+        enum class Command(val string: String) {
+            Invalid("Invalid"),
+            Push("Push"),
+            Pull("Pull"),
+            Stage("Stage"),
+            Commit("Commit"),
+            Checkout("Checkout"),
+        }
+    }
+
+    private val notificationChannel = "com.manichord.mgit.GitReceiverActivity"
     private val notificationId = 0 // what to put here?
     private lateinit var notification: NotificationCompat.Builder
 
-    class Callback(val context: Context, val receiver: Receiver) : SheimiAsyncTask.AsyncTaskCallback,
+    class Callback(val context: Context, val activity: GitReceiverActivity) : SheimiAsyncTask.AsyncTaskCallback,
         SheimiAsyncTask.AsyncTaskPostCallback {
 
         override fun doInBackground(vararg params: Void?): Boolean {
@@ -28,51 +59,50 @@ class Receiver : BroadcastReceiver() {
         override fun onPreExecute() {}
 
         override fun onProgressUpdate(vararg progress: String?) {
-            this.receiver.notification
+            this.activity.notification
                 .setContentText("${progress[0]} ${progress[2]}")
                 .setProgress(100, progress[3]!!.toInt(), false)
 
             with(NotificationManagerCompat.from(context)) {
-                notify(receiver.notificationId, receiver.notification.build())
+                notify(activity.notificationId, activity.notification.build())
             }
         }
 
         override fun onPostExecute(isSuccess: Boolean?) {
             if (isSuccess == true) {
                 with(NotificationManagerCompat.from(context)) {
-                    cancel(receiver.notificationId)
+                    cancel(activity.notificationId)
                 }
             } else {
                 // keep a notification if the task failed
-                this.receiver.notification.setContentText("Task has failed")
-                    .setStyle(NotificationCompat.BigTextStyle()
-                        .bigText("TODO placeholder for information about the failure"))
+                this.activity.notification.setContentText("Task has failed")
+                    .setProgress(0, 0, false)
+                    .setStyle(
+                        NotificationCompat.BigTextStyle()
+                            .bigText("TODO placeholder for information about the failure")
+                    )
                     .setOngoing(false)
 
                 with(NotificationManagerCompat.from(context)) {
-                    notify(receiver.notificationId, receiver.notification.build())
+                    notify(activity.notificationId, activity.notification.build())
                 }
             }
+
+            activity.finish()
         }
     }
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent == null || context == null) {
-            Log.d("mgit.Receiver", "intent and/or context are null")
-            return
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        this.notification = NotificationCompat.Builder(context, this.notificationChannel)
+        this.notification = NotificationCompat.Builder(applicationContext, this.notificationChannel)
             .setSmallIcon(R.drawable.ic_launcher)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setOnlyAlertOnce(true)
-            .setOngoing(true)
 
         val command = Command.values().firstOrNull {
-            it.string.lowercase() == intent.getStringExtra(EXTRA_COMMAND)?.lowercase()
+            it.string.lowercase() == intent.data?.host?.lowercase()
         } ?: Command.Invalid
-
-        Log.d("mgit.Receiver", "got command: $command")
 
         var repo: Repo? = null
         val id = intent.getLongExtra(EXTRA_REPO_ID, 0)
@@ -89,7 +119,7 @@ class Receiver : BroadcastReceiver() {
             }
 
             val cursor = RepoDbManager.queryAllRepo()
-            val repos = Repo.getRepoList(context, cursor)
+            val repos = Repo.getRepoList(applicationContext, cursor)
             repo = repos.firstOrNull { it.localPath == localPath }
                 ?:
                 // TODO proper error
@@ -97,7 +127,7 @@ class Receiver : BroadcastReceiver() {
         }
 
         // set the notification title to the command name
-        this.notification.setContentTitle("${repo.currentDisplayName}: ${command.string}")
+        this.notification.setContentTitle("${command.string} ${repo.localPath}")
 
         when (command) {
             Command.Pull, Command.Push -> {
@@ -121,13 +151,15 @@ class Receiver : BroadcastReceiver() {
                             remote,
                             intent.getBooleanExtra(EXTRA_PUSH_ALL, false),
                             intent.getBooleanExtra(EXTRA_FORCE, false),
-                            Callback(context, this)).executeTask()
+                            Callback(applicationContext, this)
+                        ).executeTask()
                     }
                     Command.Pull -> {
                         PullTask(repo,
                             remote,
                             intent.getBooleanExtra(EXTRA_FORCE, false),
-                            Callback(context, this)).executeTask()
+                            Callback(applicationContext, this)
+                        ).executeTask()
                     }
                     else -> {
                         throw RuntimeException("unreachable code")
@@ -136,16 +168,16 @@ class Receiver : BroadcastReceiver() {
             }
 
             Command.Commit -> {
-                // empty commit message is legal so only check for null
                 val msg = intent.getStringExtra(EXTRA_COMMIT_MSG)
-                    ?:
+                if (msg.isNullOrEmpty()) {
                     // TODO proper error
                     throw Exception("No commit message provided")
+                }
 
                 this.notification.setContentText("Commiting changes")
-                    .setOngoing(true)
+//                    .setOngoing(true)
 
-                with(NotificationManagerCompat.from(context)) {
+                with(NotificationManagerCompat.from(applicationContext)) {
                     notify(notificationId, notification.build())
                 }
 
@@ -155,7 +187,8 @@ class Receiver : BroadcastReceiver() {
                     intent.getBooleanExtra(EXTRA_STAGE_ALL, false),
                     intent.getStringExtra(EXTRA_AUTHOR_NAME),
                     intent.getStringExtra(EXTRA_AUTHOR_EMAIL),
-                    Callback(context, this)).executeTask()
+                    Callback(applicationContext, this)
+                ).executeTask()
             }
 
             Command.Stage -> {
@@ -180,14 +213,15 @@ class Receiver : BroadcastReceiver() {
                 this.notification.setContentText("???") // TODO
                     .setOngoing(true)
 
-                with(NotificationManagerCompat.from(context)) {
+                with(NotificationManagerCompat.from(applicationContext)) {
                     notify(notificationId, notification.build())
                 }
 
                 CheckoutTask(repo,
                     commit,
                     branch,
-                    Callback(context, this)).executeTask()
+                    Callback(applicationContext, this)
+                ).executeTask()
             }
 
             else -> {
@@ -196,40 +230,4 @@ class Receiver : BroadcastReceiver() {
             }
         }
     }
-
-     companion object {
-         // command to run, for list of commands look at Command enum
-         const val EXTRA_COMMAND = "command"
-
-         // local path of the repository starting from the root directory set in MGit (string)
-         const val EXTRA_REPO_LOCAL_PATH = "local_path"
-
-         // id of the repository, starting from 1 (long)
-         const val EXTRA_REPO_ID = "id"
-
-         // remote to push to, defaults to origin (string)
-         const val EXTRA_REMOTE = "remote"
-
-         // used for both push and pull (string)
-         const val EXTRA_FORCE = "force"
-
-         const val EXTRA_PUSH_ALL = "push_all"
-         const val EXTRA_STAGE_ALL = "stage_all"
-         const val EXTRA_AMEND = "amend"
-         const val EXTRA_COMMIT_MSG = "commit_msg"
-         const val EXTRA_AUTHOR_NAME = "author_name"
-         const val EXTRA_AUTHOR_EMAIL = "author_email"
-         const val EXTRA_FILE_PATTERN = "file_pattern"
-         const val EXTRA_BRANCH = "branch"
-         const val EXTRA_COMMIT = "commit"
-
-         enum class Command(val string: String) {
-             Invalid("Invalid"),
-             Push("Push"),
-             Pull("Pull"),
-             Stage("Stage"),
-             Commit("Commit"),
-             Checkout("Checkout"),
-         }
-     }
 }
