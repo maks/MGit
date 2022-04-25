d86caf4455a76100856088993991593bd5da501f
Sandorex <rzhw3h@gmail.com>
2022-04-25 16:46:02 +0200

    Added most actions It is not working now for some reason even though it worked before, it may be on my end

--- app/src/main/java/com/manichord/mgit/Receiver.kt
+++ app/src/main/java/com/manichord/mgit/Receiver.kt
@@ -3,22 +3,24 @@ package com.manichord.mgit
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.sheimi.sgit.R
 import me.sheimi.sgit.database.RepoDbManager
 import me.sheimi.sgit.database.models.Repo
 import me.sheimi.sgit.repo.tasks.SheimiAsyncTask
-import me.sheimi.sgit.repo.tasks.repo.PullTask
-import me.sheimi.sgit.repo.tasks.repo.PushTask
+import me.sheimi.sgit.repo.tasks.repo.*
 
 class Receiver : BroadcastReceiver() {
-    private val notification_channel = "com.manichord.mgit.receiver"
-    private val notification_id = 4 // what to put here?
+    private val notificationChannel = "com.manichord.mgit.Receiver"
+    private val notificationId = 0 // what to put here?
     private lateinit var notification: NotificationCompat.Builder
 
-    // TODO Make the receiver itself also a callback so there is no need to pass things around?
-    class Callback(val context: Context, val receiver: Receiver) : SheimiAsyncTask.AsyncTaskCallback {
+    class Callback(val context: Context, val receiver: Receiver) : SheimiAsyncTask.AsyncTaskCallback,
+        SheimiAsyncTask.AsyncTaskPostCallback {
+
         override fun doInBackground(vararg params: Void?): Boolean {
             return true
         }
@@ -31,124 +33,203 @@ class Receiver : BroadcastReceiver() {
                 .setProgress(100, progress[3]!!.toInt(), false)
 
             with(NotificationManagerCompat.from(context)) {
-                notify(receiver.notification_id, receiver.notification.build())
+                notify(receiver.notificationId, receiver.notification.build())
             }
         }
 
         override fun onPostExecute(isSuccess: Boolean?) {
-            with(NotificationManagerCompat.from(context)) {
-                cancel(receiver.notification_id)
-            }
+            if (isSuccess == true) {
+                with(NotificationManagerCompat.from(context)) {
+                    cancel(receiver.notificationId)
+                }
+            } else {
+                // keep a notification if the task failed
+                this.receiver.notification.setContentText("Task has failed")
+                    .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("TODO placeholder for information about the failure"))
+                    .setOngoing(false)
 
-            // TODO error if it failed?
+                with(NotificationManagerCompat.from(context)) {
+                    notify(receiver.notificationId, receiver.notification.build())
+                }
+            }
         }
     }
 
-    override fun onReceive(context: Context, intent: Intent) {
-        this.notification = NotificationCompat.Builder(context, this.notification_channel)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent == null || context == null) {
+            Log.d("mgit.Receiver", "intent and/or context are null")
+            return
+        }
+
+        this.notification = NotificationCompat.Builder(context, this.notificationChannel)
             .setSmallIcon(R.drawable.ic_launcher)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setOnlyAlertOnce(true)
             .setOngoing(true)
 
-        // TODO make these into functions so sync could just call them and not duplicate code
-        when (intent.action) {
-            "com.manichord.mgit.PULL" -> {
-                this.notification.setContentText("Pull")
+        val command = Command.values().firstOrNull {
+            it.string.lowercase() == intent.getStringExtra(EXTRA_COMMAND)?.lowercase()
+        } ?: Command.Invalid
 
-                var repo: Repo? = null
-                val id = intent.getLongExtra("id", 0)
-                if (id > 0) {
-                    repo = Repo.getRepoById(id)
-                }
+        Log.d("mgit.Receiver", "got command: $command")
 
-                if (repo == null) {
-                    val localPath = intent.getStringExtra("local_path")
-                    if (localPath.isNullOrEmpty()) {
-                        // TODO proper error
-                        throw Exception("Invalid arguments passed")
-                    }
+        var repo: Repo? = null
+        val id = intent.getLongExtra(EXTRA_REPO_ID, 0)
+        if (id > 0) {
+            // TODO check if id is valid
+            repo = Repo.getRepoById(id)
+        }
 
-                    val cursor = RepoDbManager.queryAllRepo()
-                    val repos = Repo.getRepoList(context, cursor)
+        if (repo == null) {
+            val localPath = intent.getStringExtra(EXTRA_REPO_LOCAL_PATH)
+            if (localPath.isNullOrEmpty()) {
+                // TODO proper error
+                throw Exception("Invalid repository id and/or local path")
+            }
 
-                    for (i in repos) {
-                        if (i.localPath == localPath) {
-                            repo = i
-                            break
-                        }
-                    }
-                }
+            val cursor = RepoDbManager.queryAllRepo()
+            val repos = Repo.getRepoList(context, cursor)
+            repo = repos.firstOrNull { it.localPath == localPath }
+                ?:
+                // TODO proper error
+                throw Exception("Invalid repository id and/or local path")
+        }
 
-                if (repo == null) {
-                    // TODO proper error
-                    throw Exception("Invalid arguments passed")
-                }
+        // set the notification title to the command name
+        this.notification.setContentTitle("${repo.currentDisplayName}: ${command.string}")
 
-                var remote = intent.getStringExtra("remote")
+        when (command) {
+            Command.Pull, Command.Push -> {
+                var remote = intent.getStringExtra(EXTRA_REMOTE)
                 if (remote.isNullOrEmpty()) {
-                    remote = "origin"
+                    // default to first remote
+                    remote = repo.remotes.firstOrNull()
+                        ?:
+                        // TODO proper error
+                        throw Exception("Repository contains no remotes")
                 }
 
                 if (!repo.remotes.contains(remote)) {
                     // TODO proper error
-                    throw Exception("Invalid remote passed")
+                    throw Exception("Invalid remote")
                 }
 
-                PullTask(repo,
-                    remote,
-                    intent.getBooleanExtra("force_pull", false),
+                when (command) {
+                    Command.Push -> {
+                        PushTask(repo,
+                            remote,
+                            intent.getBooleanExtra(EXTRA_PUSH_ALL, false),
+                            intent.getBooleanExtra(EXTRA_FORCE, false),
+                            Callback(context, this)).executeTask()
+                    }
+                    Command.Pull -> {
+                        PullTask(repo,
+                            remote,
+                            intent.getBooleanExtra(EXTRA_FORCE, false),
+                            Callback(context, this)).executeTask()
+                    }
+                    else -> {
+                        throw RuntimeException("unreachable code")
+                    }
+                }
+            }
+
+            Command.Commit -> {
+                // empty commit message is legal so only check for null
+                val msg = intent.getStringExtra(EXTRA_COMMIT_MSG)
+                    ?:
+                    // TODO proper error
+                    throw Exception("No commit message provided")
+
+                this.notification.setContentText("Commiting changes")
+                    .setOngoing(true)
+
+                with(NotificationManagerCompat.from(context)) {
+                    notify(notificationId, notification.build())
+                }
+
+                CommitChangesTask(repo,
+                    msg,
+                    intent.getBooleanExtra(EXTRA_AMEND, false),
+                    intent.getBooleanExtra(EXTRA_STAGE_ALL, false),
+                    intent.getStringExtra(EXTRA_AUTHOR_NAME),
+                    intent.getStringExtra(EXTRA_AUTHOR_EMAIL),
                     Callback(context, this)).executeTask()
             }
 
-            "com.manichord.mgit.PUSH" -> {
-                this.notification.setContentText("Push")
-
-                var repo: Repo? = null
-                val id = intent.getLongExtra("id", 0)
-                if (id > 0) {
-                    repo = Repo.getRepoById(id)
-                }
-
-                if (repo == null) {
-                    val localPath = intent.getStringExtra("local_path")
-                    if (localPath.isNullOrEmpty()) {
-                        // TODO proper error
-                        throw Exception("Invalid arguments passed")
-                    }
-
-                    val cursor = RepoDbManager.queryAllRepo()
-                    val repos = Repo.getRepoList(context, cursor)
-
-                    for (i in repos) {
-                        if (i.localPath == localPath) {
-                            repo = i
-                            break
-                        }
-                    }
-                }
-
-                if (repo == null) {
+            Command.Stage -> {
+                val filePattern = intent.getStringExtra(EXTRA_FILE_PATTERN)
+                if (filePattern.isNullOrEmpty()) {
                     // TODO proper error
-                    throw Exception("Invalid arguments passed")
+                    throw Exception("Invalid file pattern")
                 }
 
-                var remote = intent.getStringExtra("remote")
-                if (remote.isNullOrEmpty()) {
-                    remote = "origin"
-                }
+                AddToStageTask(repo, filePattern).executeTask()
+            }
 
-                if (!repo.remotes.contains(remote)) {
+            Command.Checkout -> {
+                val commit = intent.getStringExtra(EXTRA_COMMIT)
+                val branch = intent.getStringExtra(EXTRA_BRANCH)
+
+                if (commit.isNullOrBlank() && branch.isNullOrBlank()) {
                     // TODO proper error
-                    throw Exception("Invalid remote passed")
+                    throw Exception("Neither commit nor branch was provided for checkout command")
                 }
 
-                PushTask(repo,
-                    remote,
-                    intent.getBooleanExtra("push_all", false),
-                    intent.getBooleanExtra("force_push", false),
+                this.notification.setContentText("???") // TODO
+                    .setOngoing(true)
+
+                with(NotificationManagerCompat.from(context)) {
+                    notify(notificationId, notification.build())
+                }
+
+                CheckoutTask(repo,
+                    commit,
+                    branch,
                     Callback(context, this)).executeTask()
+            }
+
+            else -> {
+                // TODO proper error
+                throw Exception("Invalid command")
             }
         }
     }
+
+     companion object {
+         // command to run, for list of commands look at Command enum
+         const val EXTRA_COMMAND = "command"
+
+         // local path of the repository starting from the root directory set in MGit (string)
+         const val EXTRA_REPO_LOCAL_PATH = "local_path"
+
+         // id of the repository, starting from 1 (long)
+         const val EXTRA_REPO_ID = "id"
+
+         // remote to push to, defaults to origin (string)
+         const val EXTRA_REMOTE = "remote"
+
+         // used for both push and pull (string)
+         const val EXTRA_FORCE = "force"
+
+         const val EXTRA_PUSH_ALL = "push_all"
+         const val EXTRA_STAGE_ALL = "stage_all"
+         const val EXTRA_AMEND = "amend"
+         const val EXTRA_COMMIT_MSG = "commit_msg"
+         const val EXTRA_AUTHOR_NAME = "author_name"
+         const val EXTRA_AUTHOR_EMAIL = "author_email"
+         const val EXTRA_FILE_PATTERN = "file_pattern"
+         const val EXTRA_BRANCH = "branch"
+         const val EXTRA_COMMIT = "commit"
+
+         enum class Command(val string: String) {
+             Invalid("Invalid"),
+             Push("Push"),
+             Pull("Pull"),
+             Stage("Stage"),
+             Commit("Commit"),
+             Checkout("Checkout"),
+         }
+     }
 }
