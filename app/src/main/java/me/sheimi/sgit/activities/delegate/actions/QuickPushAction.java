a1a636f05755f649a4ff0b32be49f69407453d67
Chrix <w950923@gmail.com>
2022-06-26 11:38:00 +0800

    update: add quick push action for all language.

--- app/src/main/java/me/sheimi/sgit/activities/delegate/actions/QuickPushAction.java
+++ app/src/main/java/me/sheimi/sgit/activities/delegate/actions/QuickPushAction.java
@@ -29,22 +29,26 @@ public class QuickPushAction extends RepoAction {
             mActivity.showToastMessage(R.string.alert_plese_set_commit_msg_for_quick_push);
             return;
         }
+
         mActivity.closeOperationDrawer();
 
-        // stageAll, commit, push
+        // stageAll(include new file), commit, push
         AddToStageTask addTask = new AddToStageTask(mRepo, ".") {
             @Override
             protected void onPostExecute(Boolean isSuccess) {
                 super.onPostExecute(isSuccess);
                 //commit
                 CommitChangesTask commitTask = new CommitChangesTask(mRepo,
-                    quickPushMsg, false,
-                    false,
+                    quickPushMsg, false, false,
                     Profile.getUsername(mActivity.getApplicationContext()),
                     Profile.getEmail(mActivity.getApplicationContext()),
                     new AsyncTaskPostCallback() {
                         @Override
                         public void onPostExecute(Boolean isSuccess) {
+                            //   mActivity.reset() is copy from existed code when new CommitChangeTask(),
+                            // idk this line work for what, but it work bad on here, so comment.
+                            // mActivity.reset();
+
                             PushTask pushTask = new PushTask(mRepo,remotes.toArray()[0].toString(),
                                 false,false,
                                 mActivity.new ProgressCallback(R.string.push_msg_init));
