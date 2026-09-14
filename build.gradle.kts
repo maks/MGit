b12fe238f40fe6d0599d04cdad18f0bed6f427bd
Maksim Lin <maks@manichord.com>
2026-09-14 19:39:58 +1000

    get app building with updated SDK, deps

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    val kotlin_version = "2.1.0"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
